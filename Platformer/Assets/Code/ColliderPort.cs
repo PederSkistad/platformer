37e4d08a6d5374f04af225db3f0c25ea5f7e6319
PederSkistad <skiped13@nith.no>
2015-06-18 12:33:31 +0200

    La til portescript

---
+++ Platformer/Assets/Code/ColliderPort.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderPort : MonoBehaviour {
+
+
+	public Transform b;
+	public Vector3 c;
+
+
+	void OnTriggerEnter2D(Collider2D other) {
+		b.position = c;
+
+	}
+}
+
