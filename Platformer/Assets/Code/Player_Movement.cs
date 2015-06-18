0cf01b9c3178ad0190bed8e513afb19ce51e9b59
markus-rp <markus_pedersen@hotmail.com>
2015-06-18 12:41:19 +0200

    La til movement script

---
+++ Platformer/Assets/Code/Player_Movement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Movement : MonoBehaviour {
+
+	public float speed;
+	public float jumpPower;
+
+	private Rigidbody2D rb;
+	private bool grounded;
+	private bool hasJumped;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		grounded = true;
+		hasJumped = false;
+	}
+	
+	// FixedUpdate is called once per frame
+	void FixedUpdate () {
+		Movement ();
+		slowDown ();
+
+
+	}
+
+	void slowDown() {
+		if (rb.velocity != Vector2.zero && grounded) {
+			if (! Input.anyKey) {
+				rb.velocity *= (0.8f);
+			}
+		}
+	}
+
+	void Movement() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		
+		rb.AddForce (movement * speed);
+	}
+}
