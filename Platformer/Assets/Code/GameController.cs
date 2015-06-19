1a4bddfaa2167e1945615ccb55f69cb690cbbd61
PederSkistad <skiped13@nith.no>
2015-06-19 12:44:20 +0200

    lel

---
+++ Platformer/Assets/Code/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject myCanvas;
+
+	public void ChangeToScene(int sceneToChangeTo) {
+		Application.LoadLevel(sceneToChangeTo);
+	}
+	public void ChangeToScene(string sceneToChangeTo) {
+		Application.LoadLevel (sceneToChangeTo);
+	}
+	public void DisableCanvas() {
+		myCanvas.SetActive(false);
+	}
+	public void EnableCanvas()
+	{
+		myCanvas.SetActive(true);
+	}
+
+	public void pauseGame (){
+		Time.timeScale = 0.0f;
+	}
+	public void resumeGame(){
+		Time.timeScale = 1.0f;
+	}
+	public void exitGame(){
+		Application.Quit();
+	}
+}
+
---
+++ Platformer/Assets/Code/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float rotationsPerMinute;
+	void Update()
+	{
+		transform.Rotate(0,6.0*rotationsPerMinute*Time.deltaTime,0);
+}
