91282565dbdd38d6428fa241f5c52d5ac2ae785a
markus-rp <markus_pedersen@hotmail.com>
2015-07-07 14:08:17 +0200

    sick shit

---
+++ Platformer/Assets/Code/Projectiles/Proj_Straight_Line.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Proj_Straight_Line : MonoBehaviour {
+
+	public GameObject projectile;
+	public float fireRate = 2F;
+	public float projSpeed = 0.5f;
+
+	private GameObject clone;
+	private float nextFire = 0.0F;
+
+	void Start() {
+	}
+
+	void FixedUpdate() {
+
+		if (Time.time > nextFire) {
+			GameObject playR = GameObject.FindGameObjectWithTag("Player");
+			nextFire = Time.time + fireRate;
+
+			clone = (GameObject)(Instantiate(projectile, projectile.transform.position, transform.rotation));
+			Vector3 gogo = new Vector3(playR.GetComponent<Rigidbody2D>().position.x, playR.GetComponent<Rigidbody2D>().position.y, 0f);
+			clone.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(gogo);
+		}
+
+	}
+}
+
